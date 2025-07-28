5f6b23ae267ea349ea23cf66299e9b43bb910e5d
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-07-28 22:31:14 +0530

    Resolve merge conflicts in script.js

--- script.js
+++ script.js
@@ -40,23 +40,7 @@ const albumInfo = {
 
 async function getsongs(folder){
     currFolder = folder;
-<<<<<<< HEAD
     songs = songLists[folder] || [];
-    
-=======
-    let a = await fetch(`https://github.com/Eaman006/MusicoPlayer/tree/gh-pages/${folder}/`)
-    let response = await a.text();
-    let div = document.createElement("div")
-    div.innerHTML=response;
-    let as = div.getElementsByTagName("a")
-    songs=[]
-    for (let index = 0; index < as.length; index++) {
-        const element = as[index];
-        if (element.href.endsWith(".mp3")) {
-            songs.push(element.href.split(`/${folder}/`)[1])
-        }
-    }
->>>>>>> d8393195011ed5bad41c4cee8d1d9627db28cfff
     let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0]
     songUL.innerHTML=""
     for (const song of songs) {
@@ -92,42 +76,7 @@ const PlayMusic = (track, pause=false)=>{
     document.querySelector(".songtime").innerHTML="0:00/0:00"
 }
 async function displayAlbum(){
-<<<<<<< HEAD
     let cardContainer = document.querySelector(".cardContainer")
-=======
-    let a = await fetch(`https://github.com/Eaman006/MusicoPlayer/tree/gh-pages/Songs`)
-    let response = await a.text();
-    let div = document.createElement("div")
-    div.innerHTML=response;
-    let anchors = div.getElementsByTagName("a")
-    let cardContainer = document.querySelector(".cardContainer")
-    let array = Array.from(anchors)
-        for (let index = 0; index < array.length; index++) {
-            const e = array[index];
-            
-        
-        if(e.href.includes("/Songs")){
-            let folder=e.href.split("/").slice(-2)[0]
-            let a = await fetch(`https://github.com/Eaman006/MusicoPlayer/tree/gh-pages/Songs${folder}/info.json`)
-            let response = await a.json();
-            cardContainer.innerHTML=cardContainer.innerHTML+`<div data-folder="${folder}" class="card">
-            <div class="play">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                    <circle cx="12" cy="12" r="10" stroke="currentColor" stroke-width="1.5" fill="none" />
-                    <path
-                        d="M15.4531 12.3948C15.3016 13.0215 14.5857 13.4644 13.1539 14.3502C11.7697 15.2064 11.0777 15.6346 10.5199 15.4625C10.2893 15.3913 10.0793 15.2562 9.90982 15.07C9.5 14.6198 9.5 13.7465 9.5 12C9.5 10.2535 9.5 9.38018 9.90982 8.92995C10.0793 8.74381 10.2893 8.60868 10.5199 8.53753C11.0777 8.36544 11.7697 8.79357 13.1539 9.64983C14.5857 10.5356 15.3016 10.9785 15.4531 11.6052C15.5156 11.8639 15.5156 12.1361 15.4531 12.3948Z"
-                        stroke="currentColor" stroke-width="1.5" stroke-linejoin="round" />
-                </svg>
-            </div>
-            <img src="https://images.pexels.com/photos/5109665/pexels-photo-5109665.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                alt="" srcset="">
-            <h2>${response.title}</h2>
-            <p>${response.description}</p>
-        </div>`
-
-        }
-    }
->>>>>>> d8393195011ed5bad41c4cee8d1d9627db28cfff
     
     // Display albums manually
     for (const [folder, info] of Object.entries(albumInfo)) {
